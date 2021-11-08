194a5370b29370ad6ade8b89aeb7691d2b2b62cf
Elias Delgado <delgado.programming@gmail.com>
2021-11-08 10:08:08 -0300

    Sumario
    Inicio de la etapa final del proyecto
    Esto realmente lo tuve que haber hecho al principio, nota a mejorar

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -3,3 +3,8 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import Enzyme from 'enzyme';
+import { EnzymeAdapter } from 'enzyme';
+
+Enzyme.configure({ adapter: new EnzymeAdapter(),
+                    disableLifecycleMethods: true });
